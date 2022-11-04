9d38f3dc4e78df2e3811f0f1f009d79e71244afa
unknown <rmjackson234@gmail.com>
2022-11-04 16:44:37 -0600

    Rendering a HTML home page

--- byulax/views.py
+++ byulax/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def indexPageView(request) :
-    return HttpResponse('Index')
+    return render(request, 'byulax/index.html')
 
 def rosterPageView(request):
     return HttpResponse('Roster' + "git test")
