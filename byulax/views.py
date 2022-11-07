026778098fde58d75efe6527d78889f8219e33cf
Ryan Jackson <rmjackson234@gmail.com>
2022-11-07 11:27:10 -0700

    views

--- byulax/views.py
+++ byulax/views.py
@@ -6,7 +6,14 @@ def indexPageView(request) :
     return render(request, 'byulax/index.html')
 
 def rosterPageView(request):
+<<<<<<< HEAD
     return render(request, 'byulax/roster.html')    
+=======
+    return HttpResponse('Roster')
+    
+def statsPageView(request):
+    return HttpResponse('Stats')
+>>>>>>> 59c9881 (views)
 
 def statsPageView(request):
     return render(request, 'byulax/stats.html')
