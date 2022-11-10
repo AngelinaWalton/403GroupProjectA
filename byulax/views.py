f4fc018fb5ff4ad525aeb2107a6e414ba0de37bc
Ryan Jackson <rmjackson234@gmail.com>
2022-11-10 10:48:33 -0700

    Added dynamic tables in roster and schedule

--- byulax/views.py
+++ byulax/views.py
@@ -1,15 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 # Create your views here.
 def indexPageView(request) :
     return render(request, 'byulax/index.html')
 
 def rosterPageView(request):
-    return render(request, 'byulax/roster.html')    
+    data = Player.objects.all()
+    context = {
+        "player": data
+    }
+    return render(request, 'byulax/roster.html', context)    
 
 def statsPageView(request):
     return render(request, 'byulax/stats.html')
 
 def schedulePageView(request) :
-    return render(request, 'byulax/schedule.html')
+    data = Schedule.objects.all()
+    context = {
+        "game" : data
+    }
+    return render(request, 'byulax/schedule.html', context)
