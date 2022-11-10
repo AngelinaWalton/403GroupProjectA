2fb037fbac6aa8cd0e1d8e5afe20111cb063fba1
Ryan Jackson <rmjackson234@gmail.com>
2022-11-10 10:22:48 -0700

    Implemented Models and tested on Database

--- byulax/models.py
+++ byulax/models.py
@@ -1,12 +1,28 @@
 from django.db import models
 
 # Create your models here.
-class Player(models.model):
+class Player(models.Model):
     first_name = models.CharField(max_length=30)
-    last_name = models.CharField()
-    year = models.ForeignKey() #what goes in the parenthesis?
-    position = models.ForeignKey() #same as above
-    player_number = models.PositiveSmallIntegerField(max_length=3)
+    last_name = models.CharField(max_length=30)
+    FRESHMAN = 'FR'
+    SOPHOMORE = 'SO'
+    JUNIOR = 'JR'
+    SENIOR = 'SR'
+    GRADUATE = 'GR'
+    YEAR_IN_SCHOOL_CHOICES = [
+        (FRESHMAN, 'Freshman'),
+        (SOPHOMORE, 'Sophomore'),
+        (JUNIOR, 'Junior'),
+        (SENIOR, 'Senior'),
+        (GRADUATE, 'Graduate'),
+    ]
+    year_in_school = models.CharField(
+        max_length=2,
+        choices=YEAR_IN_SCHOOL_CHOICES,
+        default=FRESHMAN,
+    )
+    position = models.CharField(max_length = 3) #same as above
+    player_number = models.PositiveSmallIntegerField()
 
     @property
     def full_name(self):
@@ -14,7 +30,7 @@ class Player(models.model):
     def __str__(self):
         return (self.full_name)
 
-class Schedule(models.model):
+class Schedule(models.Model):
     date_time = models.DateTimeField()
     location = models.CharField(max_length = 40)
     opponent = models.CharField(max_length = 40)
@@ -22,6 +38,22 @@ class Schedule(models.model):
     info = models.CharField(max_length = 120, blank=True)
 
     def __str__(self):
-        return (self.Opponent + " on " + str(self.date_time)) #Im not sure this date conversion to string will work well
+        return (self.opponent + " on " + str(self.date_time)) #Im not sure this date conversion to string will work well
 
 
+class Stats(models.Model):
+    ground_ball = models.SmallIntegerField()
+    goals = models.SmallIntegerField()
+    assists = models.SmallIntegerField()
+    points = models.SmallIntegerField()
+    face_offs = models.SmallIntegerField()
+    player_id = models.ForeignKey(Player, on_delete=models.PROTECT)
+    game_id = models.ForeignKey(Schedule, on_delete= models.PROTECT)
+
+    def __str__(self):
+        return (str(self.player_id) + " and game " + str(self.game_id))
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['player_id', 'game_id'], name="game_player_CPK")
+        ]
