6b147bc853786eabb898d22aecd2de001bd81961
AngelinaWalton <95718621+AngelinaWalton@users.noreply.github.com>
2022-11-15 16:32:35 -0700

    New addStats file and edided views.py to have addStatsPage

--- byulax/views.py
+++ byulax/views.py
@@ -34,3 +34,17 @@ def addSchedulePage(request):
         new_game.save()
         return redirect('schedule')
     return render(request, 'byulax/addSchedule.html')
+
+def addStatsPage(request):
+    if request.method == 'POST':
+        new_game = Stats()
+        new_game.ground_ball = request.POST['ground_ball']
+        new_game.goals = request.POST['goals']
+        new_game.assists = request.POST['assists']
+        new_game.points = request.POST['points']
+        new_game.face_offs = request.POST['face_offs']
+        new_game.player_id = request.POST['player_id']
+        new_game.game_id = request.POST['game_id']
+        new_game.save()
+        return redirect('stats')
+    return render(request, 'byulax/addStats.html')
