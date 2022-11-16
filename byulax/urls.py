44c82a5c4733fef749a48c31a112540104e7df3b
Ryan Jackson <rmjackson234@gmail.com>
2022-11-16 11:56:20 -0700

    added delete functionality for stats and roster

--- byulax/urls.py
+++ byulax/urls.py
@@ -4,10 +4,12 @@ from .views import *
 urlpatterns = [
     path('', indexPageView, name = 'index'),
     path('roster/', rosterPageView, name = 'roster'),
-    path('roster/add',addPlayer, name="addRoster"),
+    path('roster/add/',addPlayer, name="addRoster"),
+    path('roster/delete/', deletePlayer, name="deletePlayer"),
     path('schedule/', schedulePageView, name = 'schedule'),
-    path('schedule/add', addSchedulePage, name= 'addSchedule'),
-    path('schedule/delete', deleteSchedule, name="deleteSchedule"),
+    path('schedule/add/', addSchedulePage, name= 'addSchedule'),
+    path('schedule/delete/', deleteSchedule, name="deleteSchedule"),
     path('stats/', statsPageView, name = 'stats'),
-    path('stats/add', addStatsPage, name = "addStats"),
+    path('stats/add/', addStatsPage, name = "addStats"),
+    path('stats/delete/', deleteStats, name="deleteStats"),
 ]
