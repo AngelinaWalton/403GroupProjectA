3a25ee6d85bda72c2c1f558ad1e7ade5afa049a4
christianhansen19 <chris.hansen1298@gmail.com>
2022-12-10 18:44:21 -0700

    add database

--- project1a/settings.py
+++ project1a/settings.py
@@ -13,6 +13,7 @@ import os
 from pathlib import Path
 from getpass import getpass
 from dotenv import load_dotenv
+import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -78,15 +79,10 @@ WSGI_APPLICATION = 'project1a.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+DATABASE_URL = os.getenv("DATABASE_URL")
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'byulaxdata',
-        'USER': 'postgres',
-        'PASSWORD': 10434900, #getpass(), #remember to put in a password
-        'HOST': 'localhost',
-        'PORT': "5432" #change if someone else is hosting
-    }
+    "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
 }
 
 
